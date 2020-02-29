d56f84f7399a7e9b7caf034d53b5b1537b35f8de
barakman <barakman@yahoo.com>
2020-02-29 11:30:30 +0200

    Extend the tests for function `generatePath`.

--- __tests__/paths.test.ts
+++ __tests__/paths.test.ts
@@ -13,7 +13,7 @@ describe('Path finder tests', () => {
         jest.restoreAllMocks();
     });
 
-    it('ETH to token path finder', async () => {
+    it('generatePath from ethereum token to ethereum token', async () => {
         const spyGetGraph = jest
             .spyOn(ethereum, 'getGraph')
             .mockImplementation(() => Promise.resolve({
@@ -34,8 +34,8 @@ describe('Path finder tests', () => {
             .mockImplementationOnce(() => Promise.resolve(['0']));
 
         const response = await sdk.generatePath(
-            {blockchainType: 'ethereum', blockchainId: '0xc0829421C1d260BD3cB3E0F06cfE2D52db2cE315'},
-            {blockchainType: 'ethereum', blockchainId: '0xd26114cd6EE289AccF82350c8d8487fedB8A0C07'}
+            { blockchainType: 'ethereum', blockchainId: '0xc0829421C1d260BD3cB3E0F06cfE2D52db2cE315' },
+            { blockchainType: 'ethereum', blockchainId: '0xd26114cd6EE289AccF82350c8d8487fedB8A0C07' }
         );
 
         const expectedResult = [
@@ -54,7 +54,7 @@ describe('Path finder tests', () => {
         expect(spyGetRates).toHaveBeenCalledTimes(1);
     });
 
-    it('ETH to KARMA path finder', async () => {
+    it('generatePath from ethereum token to eos token', async () => {
         const spyGetContractAddresses = jest
             .spyOn(ethereum, 'getContractAddresses')
             .mockImplementation(() => ({anchorToken: '0x1F573D6Fb3F13d689FF844B4cE37794d79a7FF1C'}));
@@ -86,8 +86,8 @@ describe('Path finder tests', () => {
             });
 
         const response = await sdk.generatePath(
-            {blockchainType: 'ethereum', blockchainId: '0xc0829421C1d260BD3cB3E0F06cfE2D52db2cE315'},
-            {blockchainType: 'eos', blockchainId: 'therealkarma', symbol: 'KARMA'}
+            { blockchainType: 'ethereum', blockchainId: '0xc0829421C1d260BD3cB3E0F06cfE2D52db2cE315' },
+            { blockchainType: 'eos', blockchainId: 'therealkarma', symbol: 'KARMA' }
         );
 
         const expectedResult = [
@@ -109,5 +109,89 @@ describe('Path finder tests', () => {
         expect(spyGetRates).toHaveBeenCalledTimes(1);
         expect(spyGetReservesFromCode).toHaveBeenCalledTimes(1);
         expect(spyGetContractAddresses).toHaveBeenCalledTimes(1);
+    });
+
+    it('generatePath from eos token to ethereum token', async () => {
+        const spyGetContractAddresses = jest
+            .spyOn(ethereum, 'getContractAddresses')
+            .mockImplementation(() => ({anchorToken: '0x1F573D6Fb3F13d689FF844B4cE37794d79a7FF1C'}));
+
+        const spyGetGraph = jest
+            .spyOn(ethereum, 'getGraph')
+            .mockImplementation(() => Promise.resolve({
+               '0xc0829421C1d260BD3cB3E0F06cfE2D52db2cE315' : ['0xb1CD6e4153B2a390Cf00A6556b0fC1458C4A5533'],
+               '0xb1CD6e4153B2a390Cf00A6556b0fC1458C4A5533' : ['0xc0829421C1d260BD3cB3E0F06cfE2D52db2cE315', '0x1F573D6Fb3F13d689FF844B4cE37794d79a7FF1C'],
+               '0x1F573D6Fb3F13d689FF844B4cE37794d79a7FF1C' : ['0xb1CD6e4153B2a390Cf00A6556b0fC1458C4A5533']
+            }));
+
+        const spyGetDecimals = jest
+            .spyOn(ethereum, 'getDecimals')
+            .mockImplementationOnce(() => Promise.resolve('18'))
+            .mockImplementationOnce(() => Promise.resolve('18'));
+
+        const spyGetRates = jest
+            .spyOn(ethereum, 'getRates')
+            .mockImplementationOnce(() => Promise.resolve(['0']));
+
+        const spyGetReservesFromCode = jest
+            .spyOn(eos, 'getReservesFromCode')
+            .mockResolvedValueOnce({
+                rows: [{
+                    contract: 'bntbntbntbnt',
+                    currency: '0.0000000000 BNT'
+                }]
+            });
+
+        const response = await sdk.generatePath(
+            { blockchainType: 'eos', blockchainId: 'therealkarma', symbol: 'KARMA' },
+            { blockchainType: 'ethereum', blockchainId: '0xc0829421C1d260BD3cB3E0F06cfE2D52db2cE315' }
+        );
+
+        const expectedResult = [
+            [
+                { blockchainType: 'eos', blockchainId: 'therealkarma', symbol: 'KARMA' },
+                { blockchainType: 'eos', blockchainId: 'bancorc11112', symbol: 'BNTKRM' },
+                { blockchainType: 'eos', blockchainId: 'bntbntbntbnt', symbol: 'BNT' }
+            ],
+            [
+                { blockchainType: 'ethereum', blockchainId: '0x1F573D6Fb3F13d689FF844B4cE37794d79a7FF1C' },
+                { blockchainType: 'ethereum', blockchainId: '0xb1CD6e4153B2a390Cf00A6556b0fC1458C4A5533' },
+                { blockchainType: 'ethereum', blockchainId: '0xc0829421C1d260BD3cB3E0F06cfE2D52db2cE315' }
+            ]
+        ];
+
+        expect(response).toEqual(expectedResult);
+        expect(spyGetGraph).toHaveBeenCalledTimes(1);
+        expect(spyGetDecimals).toHaveBeenCalledTimes(2);
+        expect(spyGetRates).toHaveBeenCalledTimes(1);
+        expect(spyGetReservesFromCode).toHaveBeenCalledTimes(1);
+        expect(spyGetContractAddresses).toHaveBeenCalledTimes(1);
+    });
+
+    it('generatePath from eos token to eos token', async () => {
+        const spyGetReservesFromCode = jest
+            .spyOn(eos, 'getReservesFromCode')
+            .mockResolvedValueOnce({
+                rows: [{
+                    contract: 'bntbntbntbnt',
+                    currency: '0.0000000000 BNT'
+                }]
+            });
+
+        const response = await sdk.generatePath(
+            { blockchainType: 'eos', blockchainId: 'therealkarma', symbol: 'KARMA' },
+            { blockchainType: 'eos', blockchainId: 'bntbntbntbnt', symbol: 'BNT' }
+        );
+
+        const expectedResult = [
+            [
+                { blockchainType: 'eos', blockchainId: 'therealkarma', symbol: 'KARMA' },
+                { blockchainType: 'eos', blockchainId: 'bancorc11112', symbol: 'BNTKRM' },
+                { blockchainType: 'eos', blockchainId: 'bntbntbntbnt', symbol: 'BNT' }
+            ]
+        ];
+
+        expect(response).toEqual(expectedResult);
+        expect(spyGetReservesFromCode).toHaveBeenCalledTimes(1);
     });
 });
