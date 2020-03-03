1edf246e0061f5beb135e5605f2cf0b747a6b77d
barakman <barakman@yahoo.com>
2020-03-03 02:37:32 +0200

    Cosmetic.

--- __tests__/paths.test.ts
+++ __tests__/paths.test.ts
@@ -13,6 +13,50 @@ describe('paths test', () => {
         jest.restoreAllMocks();
     });
 
+    it('generatePath from eos token to eos token', async () => {
+        const spyGetAnchorToken = jest
+            .spyOn(eos, 'getAnchorToken')
+            .mockImplementationOnce(() => ({ blockchainType: 'eos', blockchainId: 'aaaaaaaaaaaa', symbol: 'AAA' }));
+
+        const spyGetConvertibleTokens = jest
+            .spyOn(eos, 'getConvertibleTokens')
+            .mockImplementationOnce(() => ({ AAA: { AAACCC: 'aaacccaaaccc' } }))
+            .mockImplementationOnce(() => ({ CCC: { AAACCC: 'aaacccaaaccc' } }));
+
+        const spyGetSmartTokens = jest
+            .spyOn(eos, 'getSmartTokens')
+            .mockImplementationOnce(() => ({ }))
+            .mockImplementationOnce(() => ({ }));
+
+        const spyGetReservesFromCode = jest
+            .spyOn(eos, 'getReservesFromCode')
+            .mockResolvedValueOnce({
+                rows: [{
+                    contract: 'aaaaaaaaaaaa',
+                    currency: '0.0 AAA'
+                }]
+            });
+
+        const response = await sdk.generatePath(
+            { blockchainType: 'eos', blockchainId: 'cccccccccccc', symbol: 'CCC' },
+            { blockchainType: 'eos', blockchainId: 'aaaaaaaaaaaa', symbol: 'AAA' }
+        );
+
+        const expectedResult = [
+            [
+                { blockchainType: 'eos', blockchainId: 'cccccccccccc', symbol: 'CCC' },
+                { blockchainType: 'eos', blockchainId: 'aaacccaaaccc', symbol: 'AAACCC' },
+                { blockchainType: 'eos', blockchainId: 'aaaaaaaaaaaa', symbol: 'AAA' }
+            ]
+        ];
+
+        expect(response).toEqual(expectedResult);
+        expect(spyGetAnchorToken).toHaveBeenCalledTimes(1);
+        expect(spyGetConvertibleTokens).toHaveBeenCalledTimes(2);
+        expect(spyGetSmartTokens).toHaveBeenCalledTimes(2);
+        expect(spyGetReservesFromCode).toHaveBeenCalledTimes(1);
+    });
+
     it('generatePath from eos token to ethereum token', async () => {
         const spyGetContractAddresses = jest
             .spyOn(ethereum, 'getContractAddresses')
@@ -86,50 +130,6 @@ describe('paths test', () => {
         expect(spyGetSmartTokens).toHaveBeenCalledTimes(2);
         expect(spyGetReservesFromCode).toHaveBeenCalledTimes(1);
         expect(spyGetContractAddresses).toHaveBeenCalledTimes(1);
-    });
-
-    it('generatePath from eos token to eos token', async () => {
-        const spyGetAnchorToken = jest
-            .spyOn(eos, 'getAnchorToken')
-            .mockImplementationOnce(() => ({ blockchainType: 'eos', blockchainId: 'aaaaaaaaaaaa', symbol: 'AAA' }));
-
-        const spyGetConvertibleTokens = jest
-            .spyOn(eos, 'getConvertibleTokens')
-            .mockImplementationOnce(() => ({ AAA: { AAACCC: 'aaacccaaaccc' } }))
-            .mockImplementationOnce(() => ({ CCC: { AAACCC: 'aaacccaaaccc' } }));
-
-        const spyGetSmartTokens = jest
-            .spyOn(eos, 'getSmartTokens')
-            .mockImplementationOnce(() => ({ }))
-            .mockImplementationOnce(() => ({ }));
-
-        const spyGetReservesFromCode = jest
-            .spyOn(eos, 'getReservesFromCode')
-            .mockResolvedValueOnce({
-                rows: [{
-                    contract: 'aaaaaaaaaaaa',
-                    currency: '0.0 AAA'
-                }]
-            });
-
-        const response = await sdk.generatePath(
-            { blockchainType: 'eos', blockchainId: 'cccccccccccc', symbol: 'CCC' },
-            { blockchainType: 'eos', blockchainId: 'aaaaaaaaaaaa', symbol: 'AAA' }
-        );
-
-        const expectedResult = [
-            [
-                { blockchainType: 'eos', blockchainId: 'cccccccccccc', symbol: 'CCC' },
-                { blockchainType: 'eos', blockchainId: 'aaacccaaaccc', symbol: 'AAACCC' },
-                { blockchainType: 'eos', blockchainId: 'aaaaaaaaaaaa', symbol: 'AAA' }
-            ]
-        ];
-
-        expect(response).toEqual(expectedResult);
-        expect(spyGetAnchorToken).toHaveBeenCalledTimes(1);
-        expect(spyGetConvertibleTokens).toHaveBeenCalledTimes(2);
-        expect(spyGetSmartTokens).toHaveBeenCalledTimes(2);
-        expect(spyGetReservesFromCode).toHaveBeenCalledTimes(1);
     });
 
     it('generatePath from ethereum token to eos token', async () => {
