d83e0ffb8648860d0240d86d5e3144b0e02bd14c
barakman <barakman@yahoo.com>
2020-03-03 02:36:38 +0200

    Cosmetic.

--- __tests__/paths.test.ts
+++ __tests__/paths.test.ts
@@ -13,81 +13,6 @@ describe('paths test', () => {
         jest.restoreAllMocks();
     });
 
-    it('generatePath from ethereum token to eos token', async () => {
-        const spyGetContractAddresses = jest
-            .spyOn(ethereum, 'getContractAddresses')
-            .mockImplementationOnce(() => ({ anchorToken: '0x3333333333333333333333333333333333333333' }));
-
-        const spyGetGraph = jest
-            .spyOn(ethereum, 'getGraph')
-            .mockImplementationOnce(() => Promise.resolve({
-               '0x1111111111111111111111111111111111111111' : ['0x2222222222222222222222222222222222222222'],
-               '0x2222222222222222222222222222222222222222' : ['0x1111111111111111111111111111111111111111', '0x3333333333333333333333333333333333333333'],
-               '0x3333333333333333333333333333333333333333' : ['0x2222222222222222222222222222222222222222']
-            }));
-
-        const spyGetDecimals = jest
-            .spyOn(ethereum, 'getDecimals')
-            .mockImplementationOnce(() => Promise.resolve('18'))
-            .mockImplementationOnce(() => Promise.resolve('18'));
-
-        const spyGetRates = jest
-            .spyOn(ethereum, 'getRates')
-            .mockImplementationOnce(() => Promise.resolve(['0']));
-
-        const spyGetAnchorToken = jest
-            .spyOn(eos, 'getAnchorToken')
-            .mockImplementationOnce(() => ({ blockchainType: 'eos', blockchainId: 'aaaaaaaaaaaa', symbol: 'AAA' }))
-            .mockImplementationOnce(() => ({ blockchainType: 'eos', blockchainId: 'aaaaaaaaaaaa', symbol: 'AAA' }));
-
-        const spyGetConvertibleTokens = jest
-            .spyOn(eos, 'getConvertibleTokens')
-            .mockImplementationOnce(() => ({ AAA: { AAACCC: 'aaacccaaaccc' } }))
-            .mockImplementationOnce(() => ({ CCC: { AAACCC: 'aaacccaaaccc' } }));
-
-        const spyGetSmartTokens = jest
-            .spyOn(eos, 'getSmartTokens')
-            .mockImplementationOnce(() => ({ }))
-            .mockImplementationOnce(() => ({ }));
-
-        const spyGetReservesFromCode = jest
-            .spyOn(eos, 'getReservesFromCode')
-            .mockResolvedValueOnce({
-                rows: [{
-                    contract: 'aaaaaaaaaaaa',
-                    currency: '0.0 AAA'
-                }]
-            });
-
-        const response = await sdk.generatePath(
-            { blockchainType: 'ethereum', blockchainId: '0x1111111111111111111111111111111111111111' },
-            { blockchainType: 'eos', blockchainId: 'cccccccccccc', symbol: 'CCC' }
-        );
-
-        const expectedResult = [
-            [
-                { blockchainType: 'ethereum', blockchainId: '0x1111111111111111111111111111111111111111' },
-                { blockchainType: 'ethereum', blockchainId: '0x2222222222222222222222222222222222222222' },
-                { blockchainType: 'ethereum', blockchainId: '0x3333333333333333333333333333333333333333' }
-            ],
-            [
-                { blockchainType: 'eos', blockchainId: 'aaaaaaaaaaaa', symbol: 'AAA' },
-                { blockchainType: 'eos', blockchainId: 'aaacccaaaccc', symbol: 'AAACCC' },
-                { blockchainType: 'eos', blockchainId: 'cccccccccccc', symbol: 'CCC' }
-            ]
-        ];
-
-        expect(response).toEqual(expectedResult);
-        expect(spyGetGraph).toHaveBeenCalledTimes(1);
-        expect(spyGetDecimals).toHaveBeenCalledTimes(2);
-        expect(spyGetRates).toHaveBeenCalledTimes(1);
-        expect(spyGetAnchorToken).toHaveBeenCalledTimes(2);
-        expect(spyGetConvertibleTokens).toHaveBeenCalledTimes(2);
-        expect(spyGetSmartTokens).toHaveBeenCalledTimes(2);
-        expect(spyGetReservesFromCode).toHaveBeenCalledTimes(1);
-        expect(spyGetContractAddresses).toHaveBeenCalledTimes(1);
-    });
-
     it('generatePath from eos token to ethereum token', async () => {
         const spyGetContractAddresses = jest
             .spyOn(ethereum, 'getContractAddresses')
@@ -205,6 +130,81 @@ describe('paths test', () => {
         expect(spyGetConvertibleTokens).toHaveBeenCalledTimes(2);
         expect(spyGetSmartTokens).toHaveBeenCalledTimes(2);
         expect(spyGetReservesFromCode).toHaveBeenCalledTimes(1);
+    });
+
+    it('generatePath from ethereum token to eos token', async () => {
+        const spyGetContractAddresses = jest
+            .spyOn(ethereum, 'getContractAddresses')
+            .mockImplementationOnce(() => ({ anchorToken: '0x3333333333333333333333333333333333333333' }));
+
+        const spyGetGraph = jest
+            .spyOn(ethereum, 'getGraph')
+            .mockImplementationOnce(() => Promise.resolve({
+               '0x1111111111111111111111111111111111111111' : ['0x2222222222222222222222222222222222222222'],
+               '0x2222222222222222222222222222222222222222' : ['0x1111111111111111111111111111111111111111', '0x3333333333333333333333333333333333333333'],
+               '0x3333333333333333333333333333333333333333' : ['0x2222222222222222222222222222222222222222']
+            }));
+
+        const spyGetDecimals = jest
+            .spyOn(ethereum, 'getDecimals')
+            .mockImplementationOnce(() => Promise.resolve('18'))
+            .mockImplementationOnce(() => Promise.resolve('18'));
+
+        const spyGetRates = jest
+            .spyOn(ethereum, 'getRates')
+            .mockImplementationOnce(() => Promise.resolve(['0']));
+
+        const spyGetAnchorToken = jest
+            .spyOn(eos, 'getAnchorToken')
+            .mockImplementationOnce(() => ({ blockchainType: 'eos', blockchainId: 'aaaaaaaaaaaa', symbol: 'AAA' }))
+            .mockImplementationOnce(() => ({ blockchainType: 'eos', blockchainId: 'aaaaaaaaaaaa', symbol: 'AAA' }));
+
+        const spyGetConvertibleTokens = jest
+            .spyOn(eos, 'getConvertibleTokens')
+            .mockImplementationOnce(() => ({ AAA: { AAACCC: 'aaacccaaaccc' } }))
+            .mockImplementationOnce(() => ({ CCC: { AAACCC: 'aaacccaaaccc' } }));
+
+        const spyGetSmartTokens = jest
+            .spyOn(eos, 'getSmartTokens')
+            .mockImplementationOnce(() => ({ }))
+            .mockImplementationOnce(() => ({ }));
+
+        const spyGetReservesFromCode = jest
+            .spyOn(eos, 'getReservesFromCode')
+            .mockResolvedValueOnce({
+                rows: [{
+                    contract: 'aaaaaaaaaaaa',
+                    currency: '0.0 AAA'
+                }]
+            });
+
+        const response = await sdk.generatePath(
+            { blockchainType: 'ethereum', blockchainId: '0x1111111111111111111111111111111111111111' },
+            { blockchainType: 'eos', blockchainId: 'cccccccccccc', symbol: 'CCC' }
+        );
+
+        const expectedResult = [
+            [
+                { blockchainType: 'ethereum', blockchainId: '0x1111111111111111111111111111111111111111' },
+                { blockchainType: 'ethereum', blockchainId: '0x2222222222222222222222222222222222222222' },
+                { blockchainType: 'ethereum', blockchainId: '0x3333333333333333333333333333333333333333' }
+            ],
+            [
+                { blockchainType: 'eos', blockchainId: 'aaaaaaaaaaaa', symbol: 'AAA' },
+                { blockchainType: 'eos', blockchainId: 'aaacccaaaccc', symbol: 'AAACCC' },
+                { blockchainType: 'eos', blockchainId: 'cccccccccccc', symbol: 'CCC' }
+            ]
+        ];
+
+        expect(response).toEqual(expectedResult);
+        expect(spyGetGraph).toHaveBeenCalledTimes(1);
+        expect(spyGetDecimals).toHaveBeenCalledTimes(2);
+        expect(spyGetRates).toHaveBeenCalledTimes(1);
+        expect(spyGetAnchorToken).toHaveBeenCalledTimes(2);
+        expect(spyGetConvertibleTokens).toHaveBeenCalledTimes(2);
+        expect(spyGetSmartTokens).toHaveBeenCalledTimes(2);
+        expect(spyGetReservesFromCode).toHaveBeenCalledTimes(1);
+        expect(spyGetContractAddresses).toHaveBeenCalledTimes(1);
     });
 
     it('generatePath from ethereum token to ethereum token', async () => {
