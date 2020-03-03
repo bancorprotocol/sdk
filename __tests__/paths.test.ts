7b64acceb6af3fcc833d513455f908c3696091a3
barakman <barakman@yahoo.com>
2020-03-03 02:34:48 +0200

    Extend testing.

--- __tests__/paths.test.ts
+++ __tests__/paths.test.ts
@@ -247,4 +247,46 @@ describe('paths test', () => {
         expect(spyGetSmartTokens).toHaveBeenCalledTimes(2);
         expect(spyGetReservesFromCode).toHaveBeenCalledTimes(1);
     });
+
+    it('getAllPathsAndRates from ethereum token to ethereum token', async () => {
+        const spyGetGraph = jest
+            .spyOn(ethereum, 'getGraph')
+            .mockImplementationOnce(() => Promise.resolve({
+                '0x1111111111111111111111111111111111111111' : ['0x2222222222222222222222222222222222222222'],
+                '0x2222222222222222222222222222222222222222' : ['0x1111111111111111111111111111111111111111', '0x3333333333333333333333333333333333333333'],
+                '0x3333333333333333333333333333333333333333' : ['0x2222222222222222222222222222222222222222', '0x4444444444444444444444444444444444444444'],
+                '0x4444444444444444444444444444444444444444' : ['0x3333333333333333333333333333333333333333', '0x5555555555555555555555555555555555555555'],
+                '0x5555555555555555555555555555555555555555' : ['0x4444444444444444444444444444444444444444']
+            }));
+
+        const spyGetDecimals = jest
+            .spyOn(ethereum, 'getDecimals')
+            .mockImplementationOnce(() => Promise.resolve('4'))
+            .mockImplementationOnce(() => Promise.resolve('4'));
+
+        const spyGetRates = jest
+            .spyOn(ethereum, 'getRates')
+            .mockImplementationOnce(() => Promise.resolve(['12345678']));
+
+        const response = await sdk.getAllPathsAndRates(
+            { blockchainType: 'ethereum', blockchainId: '0x1111111111111111111111111111111111111111' },
+            { blockchainType: 'ethereum', blockchainId: '0x5555555555555555555555555555555555555555' }
+        );
+
+        const expectedResult = [{
+            path: [
+                { blockchainType: 'ethereum', blockchainId: '0x1111111111111111111111111111111111111111' },
+                { blockchainType: 'ethereum', blockchainId: '0x2222222222222222222222222222222222222222' },
+                { blockchainType: 'ethereum', blockchainId: '0x3333333333333333333333333333333333333333' },
+                { blockchainType: 'ethereum', blockchainId: '0x4444444444444444444444444444444444444444' },
+                { blockchainType: 'ethereum', blockchainId: '0x5555555555555555555555555555555555555555' }
+            ],
+            rate: '1234.5678'
+        }];
+
+        expect(response).toEqual(expectedResult);
+        expect(spyGetGraph).toHaveBeenCalledTimes(1);
+        expect(spyGetDecimals).toHaveBeenCalledTimes(2);
+        expect(spyGetRates).toHaveBeenCalledTimes(1);
+    });
 });
