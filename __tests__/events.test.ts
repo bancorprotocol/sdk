5d2d73fb84a65809b7ff4a29eb210c2d0d0996fb
barakman <barakman@yahoo.com>
2020-03-05 20:51:39 +0200

    Extend `events.test.ts`.

--- __tests__/events.test.ts
+++ __tests__/events.test.ts
@@ -43,11 +43,26 @@ describe('rates test', () => {
             });
         }
     }
+
+    for (const event of events) {
+        for (const offset of [-1, 0, +1]) {
+            it('getConversionEventsByTimestamp on ethereum', async () => {
+                setConverterVersionGetter(sdk, logs, events);
+                const received = await sdk.getConversionEventsByTimestamp({ blockchainType: 'ethereum', blockchainId: '0x'.padEnd(42, '1') }, 1, event.blockNumber + offset);
+                expect(received.length).toEqual(events.filter(e => 1 <= e.blockNumber && e.blockNumber <= event.blockNumber + offset).length);
+            });
+        }
+    }
 });
 
 function setConverterVersionGetter(sdk, logs, events) {
     sdk.ethereum.web3 = {
         eth: {
+            getBlock: function(number) {
+                if (number == "latest")
+                    number = Number.MAX_SAFE_INTEGER;
+                return {number: number, timestamp: number};
+            },
             getPastLogs: function({address: address, topics: [topic0], fromBlock: fromBlock, toBlock: toBlock}) {
                 return logs.filter(log => fromBlock <= log.blockNumber && log.blockNumber <= toBlock);
             },
