e32604107b146d62c54a6c211a4b6df30ac29df5
barakman <barakman@yahoo.com>
2020-03-05 19:54:55 +0200

    Add `events.test.ts`.

---
+++ __tests__/events.test.ts
@@ -0,0 +1,72 @@
+import { SDK } from '../src/index';
+import * as eos from '../src/blockchains/eos';
+import * as ethereum from '../src/blockchains/ethereum';
+
+describe('rates test', () => {
+    const sdk = new SDK();
+
+    const logs = Array.from(Array(10).keys()).map(n => ({
+        blockNumber: 500 + 1000 * n,
+        topics: [
+            '0x'.padEnd(66, `${3 * n + 0}`),
+            '0x'.padEnd(66, `${3 * n + 1}`),
+            '0x'.padEnd(66, `${3 * n + 2}`)
+        ]
+    }));
+
+    const events = Array.from(Array(100).keys()).map(n => ({
+        blockNumber: 1000 + 100 * n,
+        returnValues: {
+            fromToken: '0x'.padEnd(42, `${n + 1}`),
+            toToken: '0x'.padEnd(42, `${n + 2}`),
+            trader: '0x'.padEnd(42, `${n + 3}`),
+            inputAmount: `${123 * n + 1}`,
+            outputAmount: `${456 * n + 1}`,
+            conversionFee: `${789 * n + 1}`
+        }
+    }));
+
+    beforeEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    for (const event of events) {
+        for (const offset of [-1, 0, +1]) {
+            it('getConversionEvents on ethereum', async () => {
+                setConverterVersionGetter(sdk, logs, events);
+                const response = await sdk.getConversionEvents({ blockchainType: 'ethereum', blockchainId: '0x'.padEnd(42, '1') }, 1, event.blockNumber + offset);
+                expect(response.length).toEqual(events.filter(e => 1 <= e.blockNumber && e.blockNumber <= event.blockNumber + offset).length);
+            });
+        }
+    }
+});
+
+function setConverterVersionGetter(sdk, logs, events) {
+    sdk.ethereum.web3 = {
+        eth: {
+            getPastLogs: function({address: address, topics: [topic0], fromBlock: fromBlock, toBlock: toBlock}) {
+                return logs.filter(log => fromBlock <= log.blockNumber && log.blockNumber <= toBlock);
+            },
+            Contract: function(abi, address) {
+                return {
+                    getPastEvents: function(eventName, {fromBlock: fromBlock, toBlock: toBlock}) {
+                    return events.filter(event => fromBlock <= event.blockNumber && event.blockNumber <= toBlock);
+                    },
+                    methods: {
+                        decimals: function() {
+                            return {
+                                call: function() {
+                                    return '18';
+                                }
+                            };
+                        }
+                    }
+                };
+            },
+        }
+    };
+}
