da31cfd5ece0e9d01c4d59c26d6af54bb0157599
yudilevi <yudi@bancor.network>
2021-05-12 19:14:01 +0100

    removed the dist folder from git

--- dist/blockchains/ethereum/converter_events.d.ts
+++
@@ -1,3 +0,0 @@
-import { ConversionEvent, TokenRateEvent } from '../../types';
-export declare function getConversionEvents(web3: any, decimals: any, token: any, fromBlock: any, toBlock: any): Promise<ConversionEvent[]>;
-export declare function getTokenRateEvents(web3: any, decimals: any, token: any, fromBlock: any, toBlock: any): Promise<TokenRateEvent[]>;
--- dist/blockchains/ethereum/converter_version.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function get(web3: any, address: any): Promise<{
-    type: string;
-    value: string;
-}>;
